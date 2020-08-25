8eb60baf7708fcebd00f802ed4b29fa7b9b9239f
Ijaz-ali <ijaz.ali@zameen.com>
2020-08-25 21:21:27 +0500

    33. Component Nesting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import React and ReactDom Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 function getTime() {
     return (new Date().toLocaleString());
@@ -12,20 +12,7 @@ const App = () => {
     // const buttonText = "Submit Button";
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail />
         </div>
     );
 };
