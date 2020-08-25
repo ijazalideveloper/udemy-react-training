f9dc8206c6900c2592867b6799cab47176e0d93f
Ijaz-ali <ijaz.ali@zameen.com>
2020-08-25 21:03:34 +0500

    30. Specifying Images in JSX with faker library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // Import React and ReactDom Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
-function getTime(){
+function getTime() {
     return (new Date().toLocaleString());
 }
 
@@ -10,14 +11,24 @@ function getTime(){
 const App = () => {
     // const buttonText = "Submit Button";
     return (
-        <div>
-            <div>Current Time:</div>
-            <h3>
-                {getTime()}
-            </h3>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
         </div>
     );
 };
 
 // Take the react Component and show it on the screen
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
